b3043cbc79efb286c680ff726d92ea89b1e9c4c7
Nathan Marlor <nathan_marlor@hotmail.com>
2022-10-31 09:35:27 +0000

    Added Fox Cloud retry on timeout

--- custom_components/foxess_em/fox/fox_api.py
+++ custom_components/foxess_em/fox/fox_api.py
@@ -1,4 +1,5 @@
 """Fox API Client."""
+import asyncio
 import hashlib
 import logging
 
@@ -9,7 +10,11 @@ from ..util.exceptions import NoDataError
 
 _TIMEOUT = 20
 _LOGIN = "https://www.foxesscloud.com/c/v0/user/login"
-_INVALID_TOKEN = 41808
+_FOX_OK = 0
+_FOX_INVALID_TOKEN = 41808
+_FOX_TIMEOUT = 41203
+_FOX_RETRIES = 5
+_FOX_RETRY_DELAY = 10
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
@@ -25,6 +30,7 @@ class FoxApiClient:
         self._token = None
         self._fox_username = fox_username
         self._fox_password = hashlib.md5(str(fox_password).encode("utf-8")).hexdigest()
+        self._fox_retries = 0
 
     async def _refresh_token(self) -> dict:
         """Refresh login token"""
@@ -35,6 +41,8 @@ class FoxApiClient:
 
     async def async_post_data(self, url: str, params: dict[str, str]) -> dict:
         """Post data via the Fox API."""
+        self._fox_retries = 0
+
         if self._token is None:
             await self._refresh_token()
 
@@ -53,11 +61,19 @@ class FoxApiClient:
         if response.status == 200:
             result = await response.json(content_type=None)
             status = result["errno"]
-            if status == 0:
+            if status == _FOX_OK:
                 return result["result"]
-            elif status == _INVALID_TOKEN:
+            elif status == _FOX_INVALID_TOKEN:
                 _LOGGER.debug("Fox Cloud token has expired - refreshing...")
                 await self._refresh_token()
+                return await self._post_data(url, params)
+            elif status == _FOX_TIMEOUT and self._fox_retries < _FOX_RETRIES:
+                self._fox_retries += 1
+                sleep_time = self._fox_retries * _FOX_RETRY_DELAY
+                _LOGGER.debug(
+                    f"Fox Cloud timeout - retrying {self._fox_retries}/{_FOX_RETRIES} after {sleep_time}s wait..."
+                )
+                await asyncio.sleep(sleep_time)
                 return await self._post_data(url, params)
             else:
                 raise NoDataError(
