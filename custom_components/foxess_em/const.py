2ffa8198a27c8355745431aef033e0438b87e049
Nathan Marlor <nathan_marlor@hotmail.com>
2023-01-24 12:11:25 +0000

    Prevent incorrect dawn time calculations when load goes negative

--- custom_components/foxess_em/const.py
+++ custom_components/foxess_em/const.py
@@ -3,7 +3,7 @@
 NAME = "foxess_em"
 DOMAIN = "foxess_em"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.6.0b1"
+VERSION = "1.6.0b2"
 
 ISSUE_URL = "https://github.com/nathanmarlor/foxess_em/issues"
 
